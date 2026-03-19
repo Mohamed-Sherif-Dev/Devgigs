c92e3ea62dadff87cdbbd77cfc342c6a0703a636
omr2023 <momoaa792@gmail.com>
2026-03-19 14:48:00 +0200

    fix stripe api version and image config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 }
 
